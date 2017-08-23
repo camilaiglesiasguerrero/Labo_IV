108a4a6537b61a83bf09d7f0b95381478e62210f
camilaiglesiasguerrero <camila.ig08@gmail.com>
2017-08-22 21:38:07 -0300

    bd
    Corrige errores de conexión

--- APIREST-PHP-POO-JWT-MIDDLEWARE-Documentar-master/apirestV6-JWT-MW-POO/index.php
+++ APIREST-PHP-POO-JWT-MIDDLEWARE-Documentar-master/apirestV6-JWT-MW-POO/index.php
@@ -6,9 +6,11 @@ use \Psr\Http\Message\ResponseInterface as Response;
 require '../composer/vendor/autoload.php';
 require_once '/clases/AccesoDatos.php';
 require_once '/clases/cdApi.php';
+require_once '/clases/usuarioApi.php';
 require_once '/clases/AutentificadorJWT.php';
 require_once '/clases/MWparaCORS.php';
 require_once '/clases/MWparaAutentificar.php';
+
 
 $config['displayErrorDetails'] = true;
 $config['addContentLengthHeader'] = false;
